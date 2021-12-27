e832b95e91eaea266cd705be57483523495083f1
Sean K Smith <ssmith2347@gmail.com>
2021-12-27 10:47:20 -0500

    reduced notifications significantly

--- scripts/call-api.sh
+++ scripts/call-api.sh
@@ -19,3 +19,28 @@ curl -i -u test:test -H "content-type:application/json" \
 curl -i -u test:test -H "content-type:application/json" \
     -X PUT -d'{"id":"2","title": "Second Note - updated","data":"changed the contents"}' \
     http://localhost:8080/api/v1/note
+
+
+
+curl -i -u test:test -H "content-type:application/json" \
+    -X PUT -d'{"id":"1","title": "First Note","data":"somenote"}' \
+    https://notes.seanksmith.me/api/v1/note
+
+curl -i -u test:test -H "content-type:application/json" \
+    -X PUT -d'{"id":"2","title": "Second Note","data":"some other note with more info"}' \
+    https://notes.seanksmith.me/api/v1/note
+
+curl -i -u test:test -H "content-type:application/json" \
+    -X DELETE \
+    https://notes.seanksmith.me/api/v1/note/1
+
+curl -i -u test:test -H "content-type:application/json" \
+    https://notes.seanksmith.me/api/v1/note/1
+
+curl -i -u test:test -H "content-type:application/json" \
+    -X PUT -d'{"id":"2","title": "Second Note - updated","data":"some other note with more info"}' \
+    https://notes.seanksmith.me/api/v1/note
+
+curl -i -u test:test -H "content-type:application/json" \
+    -X PUT -d'{"id":"2","title": "Second Note - updated","data":"changed the contents"}' \
+    https://notes.seanksmith.me/api/v1/note
