2eb66163bc3ba5cf8e5b8d5123e3aa50b3b39a82
Sean K Smith <ssmith2347@gmail.com>
2021-12-27 12:43:27 -0500

    forcing linter failure for testing

--- cmd/main.go
+++ cmd/main.go
@@ -85,7 +85,7 @@ func configureRouter(userService user.Service, service api.NoteService) chi.Rout
 
 	r.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write([]byte("UP"))
+		w.Write([]byte("UP"))
 	})
 
 	r.Handle("/metrics", promhttp.Handler())
